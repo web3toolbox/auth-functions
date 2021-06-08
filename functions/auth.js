09163a8aee6dbbf48e79b30880b3a6ae97439e4a
Kevin Bluer <kevin@bluer.com>
2021-06-08 12:12:30 -0500

    cors

--- functions/auth.js
+++ functions/auth.js
@@ -1,16 +1,24 @@
 exports.handler = async function(event, context) {
-    
+
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+      };
+
     const users = ["xyz", "abc"];
     const user = event.queryStringParameters.user;
 
     if (users.includes(user)) {
         return {
             statusCode: 200,
+            headers,
             body: JSON.stringify({message: "Yo"})
         };
     } else {
         return {
             statusCode: 401,
+            headers,
             body: JSON.stringify({message: "Doh"})
         };
     }
