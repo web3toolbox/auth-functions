5cf9b926ae7a9c501b5c17f3bc191091349ce158
Kevin Bluer <kevin@bluer.com>
2021-06-08 12:14:52 -0500

    additional msg hash

--- functions/auth.js
+++ functions/auth.js
@@ -6,7 +6,11 @@ exports.handler = async function(event, context) {
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
       };
 
-    const users = ["xyz", "abc"];
+    const users = [
+        "xyz", 
+        "abc", 
+        "0x5c73ffc4e2bd5493db53d933c6b80f0e7c00772d83d590e8eabdedaed1c889224155b19e037175cdf0596c86a6f78e2575482dca15ea7921eaa7d81e8ade3a2a1c"
+    ];
     const user = event.queryStringParameters.user;
 
     if (users.includes(user)) {
